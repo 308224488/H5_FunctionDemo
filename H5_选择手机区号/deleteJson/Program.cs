3e73ac124873b8317cf055e02b12f56da14876fa
308224488 <308224488@qq.com>
2023-05-13 15:03:41 +0800

    选择手机区号+删除Json文件里Json对象的脚本

---
+++ H5_选择手机区号/deleteJson/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace deleteJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 读取原始的JSON文件
+            string rawData = File.ReadAllText("/Users/cailuoxiong/Desktop/deleteJson/Json/countryCode_en.json");
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(rawData);
+
+            // 在每个数组元素中删除"name_cn"属性
+            foreach (var obj in data)
+            {
+                obj.Remove("name_cn");
+            }
+            //将修改后的数据写回到文件中
+            string modifiedData = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText("countryCode_en.json", modifiedData);
+            Console.WriteLine(data);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Console.ReadKey();
+        }
+    }
+}
